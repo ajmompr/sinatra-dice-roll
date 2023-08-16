59e6642385dee745b786903b697de8e0cb2f73ce
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-16 19:32:19 +0000

    updated 1d20 die variable

--- dice.rb
+++ dice.rb
@@ -2,6 +2,12 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+# better_errors configuration
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   "<h1>Dice Roll</h1>
