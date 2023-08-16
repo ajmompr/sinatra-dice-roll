76e326e73077e3072fdc1cf96180b218d937d92c
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-16 19:11:48 +0000

    added 1/20 route

--- dice.rb
+++ dice.rb
@@ -14,3 +14,34 @@ end
 get("/giraffe") do
   "Hopefully this shows up without haveing to restart the server (**fingers crossed**)"
 end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d6</h1>
+    <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d10</h1>
+    <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+die = rand(1..20)
+
+outcome = "You rolled a #{dice} for a total of #{die}"
+
+"<h1><1d10/h1>
+  <p>#{outcome}</p>"
+end 
