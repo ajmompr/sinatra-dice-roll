c523e8c6e43ae33fb77d3cbe6401c856e2020ca6
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-20 15:21:07 +0000

    created and linked five_four view template

--- dice.rb
+++ dice.rb
@@ -27,6 +27,7 @@ get("/dice/2/6") do
   sum = first_die + second_die
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  
   erb(:two_six)
 end
 
@@ -36,28 +37,29 @@ get("/dice/2/10") do
   sum = first_die + second_die
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
   erb(:two_ten)
 end
 
 get("/dice/1/20") do
-die = rand(1..20)
+  die = rand(1..20)
 
-@outcome = "You rolled a #{die} for a total of #{die}."
-erb(:one_twenty)
+  @outcome = "You rolled a #{die} for a total of #{die}."
+ 
+  erb(:one_twenty)
 end 
 
 
 get("/dice/5/4") do
-first_die = rand(1..4)
-second_die = rand(1..4)
-third_die = rand(1..4)
-fourth_die = rand(1..4)
-fifth_die = rand(1..4)
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
 
-sum = first_die + second_die + third_die + fourth_die + fifth_die
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
 
-outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and finally, a #{fifth_die}.<br>Your total is #{sum}."
+  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and finally, a #{fifth_die}.<br>Your total is #{sum}."
 
-"<h1>5d4</h1>
-<p>#{outcome}</p>"
+  erb(:five_four)
 end 
