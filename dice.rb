8681c93cc4cb44e11f466c674bea3bd4b855e716
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-08-20 15:14:14 +0000

    created and linked one_twenty erb file

--- dice.rb
+++ dice.rb
@@ -42,10 +42,8 @@ end
 get("/dice/1/20") do
 die = rand(1..20)
 
-outcome = "You rolled a #{die} for a total of #{die}."
-
-"<h1>1d20</h1>
-  <p>#{outcome}</p>"
+@outcome = "You rolled a #{die} for a total of #{die}."
+erb(:one_twenty)
 end 
 
 
